c8d0ddd1353c4f3e5e11a3066db83113825a82b4
gaoyaocheng <309559851@qq.com>
2016-10-26 15:00:30 +0800

    change config

--- CONFIG.py
+++ CONFIG.py
@@ -1,10 +1,3 @@
-"""
-Configuration of vocabulary game server.
-Generated Wed Oct 26 14:51:45 CST 2016
-Edit to fit development or deployment environment.
-
-"""
-
 PORT=5000
 DEBUG = True  # Set to False for production use
 secret_key="080aadd8615b023b1c296cd90f65989c"
